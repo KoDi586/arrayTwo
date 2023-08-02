9d5acb76de02d41bf025f1faff5f538e0d5cd36f
1Malevolence1 <danil36rusdanil@ mail.ru>
2023-08-02 13:28:13 +0300

    started

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        task1();
+//        task2();
+//        task3();
+//        task4();
+
+    }
+    public static void task1() {
+        System.out.println("\n\n" +
+                "=============================\n" +
+                "========= Задание 1 =========\n" +
+                "=============================\n");
+    }
+}
+//новое
